142de985e195cde090c227e8d492c37fee211849
Kiyan Ang <apyoung11@hotmail.com>
2022-10-24 09:51:43 +0000

    Initial commit

---
+++ comms.py
@@ -0,0 +1,38 @@
+import paho.mqtt.client as mqtt
+from time import sleep
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected with result code " +str(rc))
+        client.subscribe("CS3237/Group_22/data")
+    else:
+        print("Connection failed with error code: %d." % rc)
+
+def on_message(client, userdata, msg):
+    print("Received message: ", end="")
+    sleep(2)
+    message = str(msg.payload.decode("utf-8"))
+    print(message)
+    result = toFahrenheit(float(message))
+    print("Sending results", end = "\n\n")
+    client.publish("CS3237/Group_22/classification", result)
+
+def setup(hostname):
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    print("Connecting")
+    client.connect(hostname, 1883, 60)
+    client.loop_start()
+    return client
+
+def main():
+    setup("broker.emqx.io")
+    while True:
+        pass
+
+def toFahrenheit(temp):
+    return round(temp * (9/5) + 32, 3)
+
+if __name__ == "__main__":
+    main()
