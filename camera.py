4c4ffd8c488ef6c3410ce27aa099982bda4012bf
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2022-10-28 20:18:41 +0800

    Finish up camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+from time import sleep
 
 frameWidth = 640
 frameHeight = 480
@@ -9,15 +10,21 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
 
-while cap.isOpened():
-    break
-    success, img = cap.read()
-    if success:
-        cv2.imshow("Result", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
 path = "images"
 if not os.path.exists(path):
     os.makedirs(path)
     print("Directory created")
+
+os.chdir(path)
+
+count = 0
+while cap.isOpened():
+    filename = "image_" + str(count) + ".png"
+    count += 1
+    success, img = cap.read()
+    if success:
+        #cv2.imshow("Result", img)
+        cv2.imwrite(filename, img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    sleep(10)
