bae0eea4f82a21741743ffb40c838ea9207c97e3
Chenhao Xiong <95895247+PbXBear@users.noreply.github.com>
2022-11-05 23:24:53 +0800

    Update dataset checking

--- compress_check.py
+++ compress_check.py
@@ -1,7 +1,8 @@
-import os
+import os, glob
 from torchvision import transforms
 from PIL import Image
 
+DATASETS = "./second_data"
 
 def transform(shape):
     w, h = shape
@@ -20,9 +21,17 @@ def compress_img(imgfile):
 
 
 if __name__ == "__main__":
-    for root, dirs, files in os.walk("./second_data"):
+    for root, dirs, files in os.walk(DATASETS):
         for file in files:
             if ".DS_Store" in file:
                 os.remove(os.path.join(root, file))
             elif file.endswith(".png"):
                 compress_img(os.path.join(root, file))
+    
+    dirs = glob.glob(os.path.join(DATASETS, "dataset_*/*/*/"))
+    for dir in dirs:
+        num_photos = len([f for f in os.listdir(dir) if f.endswith(".png")])
+        with open(os.path.join(dir, "hrData.txt")) as f:
+            num_hr = len(f.readlines())
+        if num_photos != num_hr: 
+            print(dir)
