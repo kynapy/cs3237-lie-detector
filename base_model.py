c525ae9b61cf95eeefdcf6cc876069edabe64dc1
Chenhao Xiong <95895247+PbXBear@users.noreply.github.com>
2022-11-07 11:46:26 +0800

    Update base_model.py

--- base_model.py
+++ base_model.py
@@ -9,19 +9,21 @@ from torch.backends import cudnn
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, models, transforms
+from meterbar import Summary, AverageMeter, ProgressMeter
 
 DATA_DIR = "./affectnet_hq/archive"
-RESUME_PATH = "./fer_base_model.pth.tar"
+RESUME_PATH = "./base_model.pth.tar"
 NUM_CLASSES = 8
-BATCH_SIZE = 32
-NUM_EPOCES = 50
+BATCH_SIZE = 64
+NUM_EPOCES = 30
+
 
 def load_dataset(data_dir, val_split):
     all_data = datasets.ImageFolder(
         data_dir,
         transforms.Compose([
             transforms.RandomResizedCrop(224),
-            transforms.ColorJitter(brightness=.3, hue=.3, contrast=.3, saturation=.3), 
+            transforms.ColorJitter(brightness=.3, hue=.3, contrast=.3, saturation=.3),
             transforms.RandomEqualize(),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(10),
@@ -33,16 +35,19 @@ def load_dataset(data_dir, val_split):
     train_dataset, val_dataset = random_split(all_data, [train_size, val_size])
     return train_dataset, val_dataset
 
+class BaseModel(nn.Module):
+    def __init__(self, num_classes, pretrain=True):
+        super().__init__()
+        weights = "DEFAULT" if pretrain else None
+        self.model = models.regnet_y_400mf(weights=weights)
+        self.model.fc = nn.Linear(440, num_classes)
 
-def create_model(num_classes, pretrain=True):
-    weights = models.RegNet_Y_1_6GF_Weights.IMAGENET1K_V2 if pretrain else None
-    model = models.regnet_y_1_6gf(weights=weights)
-    model.fc = nn.Linear(888, num_classes)
-    return model
+    def forward(self, img):
+        return self.model(img)
 
 
 def main_worker(data_dir, num_classes, batch_size, epochs, resume=None, workers=0):
-    model = create_model(num_classes).to(device)
+    model = BaseModel(num_classes).to(device)
     criterion = nn.CrossEntropyLoss().to(device)
     optimizer = optim.AdamW(model.parameters(), 3e-4, weight_decay=5e-4)
     scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
@@ -151,88 +156,8 @@ def validate(val_loader, model, criterion, print_freq=80):
 def save_checkpoint(state, filename, is_best=False):
     torch.save(state, filename)
     if is_best:
-        best_path = os.path.splitext(os.path.splitext(filename)[0])[0]+"_best.pth.tar"
+        best_path = os.path.splitext(os.path.splitext(filename)[0])[0] + "_best.pth.tar"
         shutil.copyfile(filename, best_path)
-
-
-class Summary(Enum):
-    NONE = 0
-    AVERAGE = 1
-    SUM = 2
-    COUNT = 3
-
-
-class AverageMeter(object):
-
-    def __init__(self, name, fmt=':f', summary_type=Summary.AVERAGE):
-        self.name = name
-        self.fmt = fmt
-        self.summary_type = summary_type
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-    def all_reduce(self):
-        if torch.cuda.is_available():
-            device = torch.device("cuda")
-        else:
-            device = torch.device("cpu")
-        total = torch.tensor([self.sum, self.count], dtype=torch.float32, device=device)
-        distributed.all_reduce(total, distributed.ReduceOp.SUM, async_op=False)
-        self.sum, self.count = total.tolist()
-        self.avg = self.sum / self.count
-
-    def __str__(self):
-        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
-        return fmtstr.format(**self.__dict__)
-
-    def summary(self):
-        fmtstr = ''
-        if self.summary_type is Summary.NONE:
-            fmtstr = ''
-        elif self.summary_type is Summary.AVERAGE:
-            fmtstr = '{name} {avg:.3f}'
-        elif self.summary_type is Summary.SUM:
-            fmtstr = '{name} {sum:.3f}'
-        elif self.summary_type is Summary.COUNT:
-            fmtstr = '{name} {count:.3f}'
-        else:
-            raise ValueError('invalid summary type %r' % self.summary_type)
-
-        return fmtstr.format(**self.__dict__)
-
-
-class ProgressMeter(object):
-
-    def __init__(self, num_batches, meters, prefix=""):
-        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
-        self.meters = meters
-        self.prefix = prefix
-
-    def display(self, batch):
-        entries = [self.prefix + self.batch_fmtstr.format(batch)]
-        entries += [str(meter) for meter in self.meters]
-        print('\t'.join(entries))
-
-    def display_summary(self):
-        entries = [" *"]
-        entries += [meter.summary() for meter in self.meters]
-        print(' '.join(entries))
-
-    def _get_batch_fmtstr(self, num_batches):
-        num_digits = len(str(num_batches // 1))
-        fmt = '{:' + str(num_digits) + 'd}'
-        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
 
 
 def accuracy(output, target, topk=(1, )):
@@ -251,7 +176,7 @@ def accuracy(output, target, topk=(1, )):
         return res
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     if torch.cuda.is_available():
         print("Using GPU.")
         device = torch.device("cuda")
