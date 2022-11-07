f6b6cbc59da43559b56931974232a466476f5900
Chenhao Xiong <95895247+PbXBear@users.noreply.github.com>
2022-11-07 11:45:05 +0800

    Update optimal_flow.py

--- optimal_flow.py
+++ optimal_flow.py
@@ -3,7 +3,16 @@ import numpy as np
 import dlib
 
 
-class face_landmarks:
+def tensor2cv2(img):
+    return np.transpose((img * 255 + 0.5).clip(0, 255).astype(np.uint8), (1, 2, 0))
+
+
+def normalize(img):
+    m, s = img.mean(), img.std()
+    return (img - m) / s
+
+
+class face_landmarks(object):
 
     def __init__(self):
         self.detector = dlib.get_frontal_face_detector()
@@ -11,21 +20,31 @@ class face_landmarks:
         self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
     def __call__(self, img):
-        faces = self.detector(img)
+        cv_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        faces = self.detector(cv_img)
         pos = []
         for face in faces:
-            landmarks = self.predictor(image=img, box=face)
+            landmarks = self.predictor(image=cv_img, box=face)
             for n in range(self.num_marks):
                 x, y = landmarks.part(n).x, landmarks.part(n).y
                 pos.append([x, y])
-        return pos
+        return np.array(pos)
 
 
-class dense_flow:
+class dense_flow(object):
 
     def __init__(self):
         self.RLOF = cv2.optflow.DenseRLOFOpticalFlow_create()
+        self.TVL1 = cv2.optflow.DualTVL1OpticalFlow_create()
 
-    def __call__(self, prev, next):
-        flow = self.RLOF.calc(prev, next, None)
+    def __call__(self, prev, next, opt="RLOF"):
+        cv_prev = cv2.cvtColor(prev, cv2.COLOR_RGB2BGR)
+        cv_next = cv2.cvtColor(next, cv2.COLOR_RGB2BGR)
+        if opt == "RLOF":
+            flow = self.RLOF.calc(cv_prev, cv_next, None)
+        elif opt == "TVL1":
+            cv_prev_gray = cv2.cvtColor(cv_prev , cv2.COLOR_BGR2GRAY)
+            cv_next_gray = cv2.cvtColor(cv_next, cv2.COLOR_BGR2GRAY)
+            flow = self.TVL1.calc(cv_prev_gray, cv_next_gray, None)
+        else: ValueError()
         return np.transpose(flow, (2, 0, 1))
